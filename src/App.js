369893a5c88992cf64868edea5ff2a5831c5311e
Jaizelle Argueta <jaizelle@gmail.com>
2020-02-24 13:28:46 -0500

    Fixed click counter

--- src/App.js
+++ src/App.js
@@ -27,26 +27,44 @@ class Selector extends Component {
         <div className="navbar">
           <div>Selected: <span>{this.state.selectedShape}</span></div>
         </div>
-        <div className="shape-list" onClick={this.countClicks}>
-
+        <div className="shape-list">
+          <div onClick={this.countClicks}>
           <Shape shape="square" selectShape={this.selectShape}
-           />
+           /></div>
+           <div onClick={this.countClicks}>
           <Shape shape="triangle" selectShape={this.selectShape}/>
+          </div>
+          <div onClick={this.countClicks}>
           <Shape shape="circle" selectShape={this.selectShape}/>
+          </div>
         </div>
 
-        <div className="shape-list" onClick={this.countClicks}>
+        <div className="shape-list">
+          <div onClick={this.countClicks}>
           <Shape shape="oval" selectShape={this.selectShape}/>
+          </div>
+          <div onClick={this.countClicks}>
           <Shape shape="trapezium" selectShape={this.selectShape}/>
+          </div>
+          <div onClick={this.countClicks}>
           <Shape shape="triangle_down" selectShape={this.selectShape}/>
+          </div>
         </div>
 
-        <div className="shape-list" onClick={this.countClicks}>
+        <div className="shape-list">
+          <div onClick={this.countClicks}>
           <Shape shape="star" selectShape={this.selectShape}/>
+          </div>
+          <div onClick={this.countClicks}>
           <Shape shape="triangle_left" selectShape={this.selectShape}/>
+          </div>
+          <div onClick={this.countClicks}>
           <Shape shape="triangle_right" selectShape={this.selectShape}/>
+          </div>
         </div>
+
         <div className="footer">Total clicks: {this.state.count}</div>
+  
       </div>
     )
   }
